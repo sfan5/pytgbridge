6e134dfcdd7121fd555ad1ac6ba5b2977840463c
sfan5 <sfan5@live.de>
2016-10-17 19:20:01 +0200

    Add built-in HTTP web_backend (mainly for dev purposes)

--- src/web_backend.py
+++ src/web_backend.py
@@ -1,6 +1,17 @@
 import os
 import urllib
 import logging
+# for built-in HTTP server:
+import threading
+import tempfile
+import http.server
+import socketserver
+
+def http_server_thread(host, port, wwwpath):
+	os.chdir(wwwpath)
+	serv = socketserver.TCPServer((host, port), http.server.SimpleHTTPRequestHandler)
+	logging.info("Built-in HTTP server listening on %s:%d, dir: %s", host, port, wwwpath)
+	serv.serve_forever()
 
 def urlopen(url, headers={}):
 	r = urllib.request.Request(url, headers=headers)
@@ -25,11 +36,21 @@ class WebBackend():
 			self.webpath = config["webpath"]
 			self.baseurl = config["baseurl"]
 			self.subdirs = config["use_subdirs"]
+		elif self.type == "builtin":
+			bind = "127.0.0.1" if "bind" not in config else config["bind"]
+			port = config["port"]
+			self.subdirs = config["use_subdirs"]
+			# Used by download_and_serve():
+			self.webpath = tempfile.mkdtemp()
+			self.baseurl = "http://%s:%d" % (bind, port)
+			t = threading.Thread(target=http_server_thread, args=(bind, port, self.webpath))
+			t.start()
 		elif self.type == "stub":
 			logging.warning("Web backend not functional! (stub)")
 		else:
 			logging.error("Unknown web backend type")
 			exit(1)
+
 	@staticmethod
 	def _hash(s):
 		v = 0
@@ -37,8 +58,9 @@ class WebBackend():
 			v += ord(c)
 			v = (v << 1) & 0xffff | (v >> 15) # ROR by 1 bit
 		return v
+
 	def _filepath(self, filename):
-		assert(self.type == "external")
+		assert(self.type in ("external", "builtin"))
 		if not self.subdirs:
 			return filename
 		h = WebBackend._hash(filename)
@@ -48,7 +70,7 @@ class WebBackend():
 		except FileExistsError:
 			pass
 		return h + "/" + filename
-		
+
 	def download_and_serve(self, url, filename=None):
 		if self.type == "stub":
 			return "<no link available>"
@@ -56,3 +78,4 @@ class WebBackend():
 		with open(self.webpath + "/" + filepath, "wb") as f:
 			download_file(url, f)
 		return self.baseurl + "/" + filepath
+
