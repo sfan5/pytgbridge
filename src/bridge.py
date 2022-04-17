54045cc8a647087efb8cd9f495122c5bf059df37
cactushydrocodone <12892508+cactushydrocodone@users.noreply.github.com>
2022-04-17 19:29:46 +0200

    Use empty list if the option is not defined

--- src/bridge.py
+++ src/bridge.py
@@ -194,7 +194,7 @@ class Bridge():
 		self.web = wb
 		#
 		self.links = set(LinkTuple(**e) for e in config["links"])
-		self.ignored_users = set(config["ignored_telegram_users"])
+		self.ignored_users = set(config["ignored_telegram_users"]) if "ignored_telegram_users" in config else []
 		logging.info("%d link(s) configured", len(self.links))
 		options = config_defaults.copy()
 		options.update(config["options"])
